c36877da6087dd5d7fc32f02694e0897b3e6f724
xh24 <samuel.schnoedt@web.de>
2026-07-17 18:52:21 +0200

    supabase.js aktualisieren

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://qkfvtsrjxombytdvtkja.supabase.co";
 
-const supabaseKey = "DEIN_ANON_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZnZ0c3JqeG9tYnl0ZHZ0a2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDAxMzAsImV4cCI6MjA5OTg3NjEzMH0.yG8GdTRNkDzd_wEIOJXZjxibqfSWkXJrg706xPP_i2g";
 
 export const supabase = createClient(
     supabaseUrl,
